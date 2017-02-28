8192879478e5836e36e361371cdf9b31afd150c5
buhi <yinshang1992@gmail.com>
2017-02-28 13:54:08 +0800

    build directory should not contain index.*

--- build/index.d.ts
+++
@@ -1,7 +0,0 @@
-import "../Grid.css";
-export { Grid as ReduxAgGrid, GridFieldSchema, setStore } from "./Grid";
-export { RestfulResource } from "./RestfulResource";
-export { GridReducer } from "./GridReducer";
-export { GridStore } from "./Store";
-export { keyValueToQueryParams, deepSetState, deepGetState, setImmuOrPOJO, getImmuOrPOJO } from "./Utils";
-export { ITheme, setTheme } from "./themes";
